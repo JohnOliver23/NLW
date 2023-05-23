3308a3997e14854895d339f4c00e6db3f5c7f3bf
Jhon Oliver <johnoliver021@gmail.com>
2023-05-22 23:43:12 -0300

    NLW - SPACETIME - Iniciando configuracao de rotas no app

--- spacetime/mobile/app/_layout.tsx
+++ spacetime/mobile/app/_layout.tsx
@@ -4,20 +4,32 @@ import {
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto'
-import { Slot, SplashScreen } from 'expo-router'
+import { SplashScreen, Stack } from 'expo-router'
+import * as SecureStore from 'expo-secure-store'
 import { StatusBar } from 'expo-status-bar'
 import { styled } from 'nativewind'
+import { useEffect, useState } from 'react'
 import { ImageBackground } from 'react-native'
 import blurBg from '../src/assets/bg-blur.png'
 import Stripes from '../src/assets/stripes.svg'
 const StyledStripes = styled(Stripes)
 
 export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<
+    null | boolean
+  >(null)
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) => {
+      setIsUserAuthenticated(!!token)
+    })
+  }, [])
 
   if (!hasLoadedFonts) {
     return <SplashScreen />
@@ -34,7 +46,16 @@ export default function Layout() {
     >
       <StyledStripes className="absolute left-2" />
       <StatusBar style="light" translucent />
-      <Slot />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: 'transparent' },
+        }}
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="new" />
+        <Stack.Screen name="memories" />
+      </Stack>
     </ImageBackground>
   )
 }
