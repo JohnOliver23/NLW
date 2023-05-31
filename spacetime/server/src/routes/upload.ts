af65ec36cd4c6317df1946b9b9415d1ce8aa4f46
unknown <john.andrade@tipbank.com.br>
2023-05-30 21:16:42 -0300

    NLW - SPACETIME - Finalizando upload de arquivo no backend

--- spacetime/server/src/routes/upload.ts
+++ spacetime/server/src/routes/upload.ts
@@ -1,4 +1,11 @@
+import { randomUUID } from "crypto";
 import { FastifyInstance } from "fastify";
+import { createWriteStream } from "node:fs";
+import { extname, resolve } from "node:path";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
+
+const pump = promisify(pipeline)
 
 export async function uploadRoutes(app: FastifyInstance,) {
   app.post('/upload', async(request, reply) => {
@@ -17,7 +24,20 @@ export async function uploadRoutes(app: FastifyInstance,) {
       return reply.status(400).send()
     }
 
-    console.log(upload.filename)
+    const fileId = randomUUID()
+    const extension = extname(upload.filename)
+
+    const fileName = fileId.concat(extension)
+
+    const writeStream = createWriteStream(
+      resolve(__dirname, '../../uploads', fileName)
+    )
+
+    await pump(upload.file, writeStream)
+
+    const fullUrl = request.protocol.concat('://').concat(request.hostname)
+    const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
+    return {fileUrl}
     
   })
 
--- spacetime/server/src/server.ts
+++ spacetime/server/src/server.ts
@@ -4,6 +4,7 @@ import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import multipart from '@fastify/multipart'
 import fastify from 'fastify'
+import { resolve } from 'node:path'
 import { authRotes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 import { uploadRoutes } from './routes/upload'
@@ -11,6 +12,11 @@ import { uploadRoutes } from './routes/upload'
 const app = fastify()
 
 app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads'
+})
 
 app.register(cors, {
   origin: true
